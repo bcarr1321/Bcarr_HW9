ad2ac5d65beb6d6253940016dab9baa7bd5ada5a
bcarr1321 <bcarr1@uiowa.edu>
2021-03-23 19:08:42 -0500

    Lorum Sipum added to home page

--- app.py
+++ app.py
@@ -7,9 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html')
+@app.route('/About')
+def About():
+    return render_template('About.html')
 
+@app.route('/Estimate', methods=['GET'])
+def Estimate():
+    return render_template('Estimate.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        num1 = int(request.form['radius'])
+        num2 = int(request.form['height'])
+        
 if __name__ == '__main__':
     app.run(debug=True)
